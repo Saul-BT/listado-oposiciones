e0ab8bab32ceb815b00150a10f58987ee7792c12
Alex <alex.velazquez.fernandez@iesjulianmarias.es>
2019-05-23 10:09:04 +0200

    Creando Modelo Tabla (incompleto).

--- ProyectoOposiciones/src/proyectooposiciones/ModeloTabla.java
+++ ProyectoOposiciones/src/proyectooposiciones/ModeloTabla.java
@@ -5,10 +5,66 @@
  */
 package proyectooposiciones;
 
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
 /**
  *
  * @author dam102
  */
-public class ModeloTabla {
+public class ModeloTabla extends DefaultTableModel {
+    private Persona[] lasPersonas;
+
+    public ModeloTabla(Persona[] lasPersonas) {
+        this.lasPersonas = lasPersonas;
+    }
+    
+    
+
+    @Override
+    public int getRowCount() {
+        return lasPersonas == null ? 0 : lasPersonas.length;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 6;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        String[] nombres = {
+            "Nº Opo.",
+            "NIF",
+            "Apellidos y Nombre",
+            "Ejer Parte I",
+            "Ejer Parte II",
+            "Total"
+        };
+        return nombres[columnIndex];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        Class<?>[] clases = {
+            
+        };
+        return clases[columnIndex];
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Persona persona = lasPersonas[rowIndex];
+        if (persona == null) return null;
+        Object[] informacionPersona = {
+            
+        };
+    }
     
 }
